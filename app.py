c44bcc6831ae13158ad8cb2709f54cc0aaa4c51c
Jr Stan <66187962+standre-hue@users.noreply.github.com>
2022-02-13 09:11:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ class Livre(db.Model):
 
 #db.create_all()
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+#port = int(os.environ.get("PORT", 5000))
+#app.run(host="0.0.0.0", port=port)
 # s Livre(db.Model):
-#app.run(debug=True)
+app.run(debug=True)
